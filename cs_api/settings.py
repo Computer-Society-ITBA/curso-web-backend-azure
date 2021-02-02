e96de8341196a3bd430f4ae4e05e692326aa4baa
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-02-02 12:23:55 -0300

    Removed debug

--- cs_api/settings.py
+++ cs_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGOSECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 USE_TZ = True
 
